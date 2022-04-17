1ad3082fbf243e211b96773f8e27fdfe51b5673a
al-anas <alanas7171@gmail.com>
2022-04-17 20:28:38 +0600

    email and password authentication implemented in login component

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,54 +1,98 @@
 import React, { useState } from "react";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
+import auth from "../../firebase.init";
 
 const Login = () => {
   const [check, setCheck] = useState(false);
+  const [createUserWithEmailAndPassword, user1, loading1, error1] =
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
-  const handleCheck = () =>{
-      setCheck(!check);
-  }
+  const [signInWithEmailAndPassword, user2, loading2, error2] =
+    useSignInWithEmailAndPassword(auth);
+
+  const navigate = useNavigate();
+
+  const handleCheck = () => {
+    setCheck(!check);
+  };
+
+   if(user2){
+      navigate('/home');
+   }
+
+
+  const handleFormSubmit = (event) => {
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+
+    if (!check) {
+      createUserWithEmailAndPassword(email, password);
+    } 
+    else {
+      signInWithEmailAndPassword(email, password)
+    }
+
+    event.preventDefault();
+  };
 
   return (
     <div className="w-50 mx-auto my-5 border p-4">
-      <h2>Please {check?'Login':'Registration'}</h2>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
+      <h2>Please {check ? "Login" : "Registration"}</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
+            name="email"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            required
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
             Password
           </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
+            name="password"
             id="exampleInputPassword1"
+            required
           />
         </div>
-        <div class="mb-3 form-check">
-          <input onClick={handleCheck} type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
+        <div className="mb-3 form-check">
+          <input
+            onClick={handleCheck}
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
             Already have an account?
           </label>
         </div>
-        {
-          check ? (
-          <button type="submit" class="btn btn-primary">
+        {check ? (
+          <button type="submit" className="btn btn-primary">
             Login
           </button>
         ) : (
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             Registration
           </button>
         )}
       </form>
+      <p>{error1 && error1.message}</p>
+      <p>{error2 && error2.message}</p>
     </div>
   );
 };
